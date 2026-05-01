80894655b74b5068cfaa0399912d9dd43e13852d
BandwarShlok <bandwarshlok@gmail.com>
2026-05-01 15:03:58 +0530

    git push

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/student_result_system");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB Error:", error.message);
+    console.error("MongoDB Error:", error);
     process.exit(1);
   }
 };
