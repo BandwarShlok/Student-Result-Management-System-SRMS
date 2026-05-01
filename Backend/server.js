18359d2906f94ac6d4080aa4dc377e052808ee17
BandwarShlok <bandwarshlok@gmail.com>
2026-05-01 14:56:52 +0530

    fix port issue

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,8 @@ app.use("/api/results", resultRoutes);
 app.use("/api/admin", adminRoutes);
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
